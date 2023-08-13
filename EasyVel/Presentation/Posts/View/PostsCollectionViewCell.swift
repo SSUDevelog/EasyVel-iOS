0439503273f0e1a8554ee781fbc76b302e5cd13b
seongmin221 <seongmin221@naver.com>
2023-08-14 02:43:25 +0900

    [CHORE] changing PostsTableView to PostsCollectionView with diffable data source

--- EasyVel/Presentation/Posts/View/PostsCollectionViewCell.swift
+++ EasyVel/Presentation/Posts/View/PostsCollectionViewCell.swift
@@ -5,4 +5,91 @@
 //  Created by 이성민 on 2023/08/13.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+
+final class PostsCollectionViewCell: BaseCollectionViewCell {
+    
+    static let identifier = "PostsCollectionViewCell"
+    
+    // MARK: - Property
+    
+    
+    
+    // MARK: - UI Property
+    
+    private let imageView: UIImageView = {
+        let view = UIImageView()
+        view.contentMode = .scaleAspectFill
+        view.layer.cornerRadius = 8
+        view.clipsToBounds = true
+        return view
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .gray700
+        label.font = .subhead
+        return label
+    }()
+    
+    private let textView: UITextView = {
+        let view = UITextView()
+        view.textColor = .gray500
+        view.isEditable = false
+        view.isSelectable = false
+        view.isScrollEnabled = false
+        view.font = .body_1_M
+        return view
+    }()
+    
+    private let dateLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .gray300
+        label.font = .caption_1_M
+        return label
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .gray300
+        label.font = .caption_1_M
+        return label
+    }()
+    
+    let scrapButton: UIButton = {
+        let button = UIButton()
+        button.setImage(ImageLiterals.bookMark, for: .normal)
+        // FIXME: tap scrapbutton action
+        return button
+    }()
+    
+    private let firstTag: PostTagUIButton = PostTagUIButton()
+    private let secondTag: PostTagUIButton = PostTagUIButton()
+    private let thirdTag: PostTagUIButton = PostTagUIButton()
+    private lazy var tagStackView: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [firstTag, secondTag, thirdTag])
+        view.spacing = 6
+        view.axis = .horizontal
+        return view
+    }()
+    
+    // MARK: - Life Cycle
+    
+    
+    
+    // MARK: - Setting
+    
+    
+    
+    // MARK: - Action Helper
+    
+    
+    
+    // MARK: - Custom Method
+    
+    
+    
+    
+}
--- EasyVel/Presentation/Posts/View/PostsDataSource.swift
+++ EasyVel/Presentation/Posts/View/PostsDataSource.swift
@@ -5,4 +5,70 @@
 //  Created by 이성민 on 2023/08/14.
 //
 
-import Foundation
+import UIKit
+
+final class PostsDataSource {
+    
+    typealias PostCell = PostsCollectionViewCell
+    typealias DataSource = UICollectionViewDiffableDataSource
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Section, PostDTO.ID>
+    
+    // MARK: - Property
+    
+    enum Section {
+        case main
+    }
+    
+    private var dataSource: DataSource<Section, PostDTO.ID>!
+    private let collectionView: UICollectionView
+    private let postsList: [PostDTO]
+    
+    // MARK: - UI Property
+    
+    init(
+        collectionView: UICollectionView,
+        posts: [PostDTO]
+    ) {
+        self.collectionView = collectionView
+        self.postsList = .init()
+        
+        setCellRegistration()
+        setDataSource()
+    }
+    
+    // MARK: - Life Cycle
+    
+    
+    
+    // MARK: - Setting
+    
+    private func setCellRegistration() {
+        self.collectionView.register(cell: PostCell.self)
+    }
+    
+    private func setDataSource() {
+        let cellRegistration = UICollectionView.CellRegistration<PostCell, PostDTO> { cell, indexPath, post in
+            // FIXME: Cell configuration
+        }
+        
+        dataSource = DataSource(collectionView: collectionView, cellProvider: { collectionView, indexPath, id in
+            let post = self.postsList[indexPath.item]
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration,
+                                                                for: indexPath,
+                                                                item: post)
+        })
+    }
+    
+    // MARK: - Custom Method
+    
+    func loadPosts(
+        _ incomingPosts: [PostDTO]
+    ) {
+        let postIDs = incomingPosts.map { $0.id }
+        var snapshot = Snapshot()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(postIDs, toSection: .main)
+        dataSource.applySnapshotUsingReloadData(snapshot)
+    }
+    
+}
