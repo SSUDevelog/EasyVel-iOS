355a21eb8c1893aafd20884f860a0a83f2c17726
seongmin221 <seongmin221@naver.com>
2023-09-21 13:41:04 +0900

    [FEAT] #3 - editing newpostVC in progress

--- EasyVel/Presentation/Posts/NewPostsViewController.swift
+++ EasyVel/Presentation/Posts/NewPostsViewController.swift
@@ -11,17 +11,33 @@ import RxSwift
 import RxRelay
 import RxCocoa
 
+enum ViewType {
+    case trend
+    case subscriber
+    case tag
+}
+
 final class NewPostsViewController: RxBaseViewController<NewPostsViewModel> {
+    
+    typealias PostCell = PostsCollectionViewCell
+    typealias DataSource = UICollectionViewDiffableDataSource<Section, PostDTO.ID>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Section, PostDTO.ID>
+    
+    enum Section {
+        case main
+    }
     
     // MARK: - Property
     
     private var posts: [PostDTO]?
-    private var isScrapPostsList: [Bool]?
+    //    private var isScrapPostsList: [Bool]?
     private var isNavigationBarHidden: Bool?
     
     // MARK: - UI Property
     
     private let postsView = PostsView()
+    private var postsDataSource: DataSource!
+    private var postsSnapshot: Snapshot!
     
     // MARK: - Life Cycle
     
@@ -34,42 +50,40 @@ final class NewPostsViewController: RxBaseViewController<NewPostsViewModel> {
     
     init(
         viewModel: NewPostsViewModel,
-        isNavigationBarHidden: Bool,
-        posts: [PostDTO]
+        posts: [PostDTO],
+        isNavigationBarHidden: Bool
     ) {
         super.init(viewModel: viewModel)
-        self.isNavigationBarHidden = isNavigationBarHidden
         self.posts = posts
+        self.isNavigationBarHidden = isNavigationBarHidden
         self.view = postsView
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
         navigationBarIsHidden(isNavigationBarHidden ?? true)
     }
     
     // MARK: - Setting
     
     override func bind(viewModel: NewPostsViewModel) {
-        let reload = postsView.postsCollectionView.refreshControl!.rx
-            .controlEvent(.valueChanged)
-            .asObservable()
+        self.confiugreDataSource()
+        //        let reload = postsView.collectionView.refreshControl?.rx
+        //            .controlEvent(.valueChanged)
+        //            .asObservable()
         let viewWillAppear = viewModel.viewWillAppear
             .asObservable()
         
-        let postTrigger = Observable.merge(reload, viewWillAppear)
+        //        let postTrigger = Observable.merge(reload, viewWillAppear)
         
-        let input = NewPostsViewModel.Input(
-            postTrigger: postTrigger,
-//            scrapButtonTapped: scrapButtonTapped
-        )
+        let input = NewPostsViewModel.Input(viewWillAppear)
         
         let output = viewModel.transform(input: input)
         
-        output.postList
-            .map { $0.map { $0.0 } }
-
-        
+        output.postList.drive(onNext: {
+            self.loadSnapshotData(with: $0)
+        }).disposed(by: disposeBag)
     }
     
     // MARK: - Action Helper
@@ -83,5 +97,51 @@ final class NewPostsViewController: RxBaseViewController<NewPostsViewModel> {
 // MARK: - DatatSource
 
 extension NewPostsViewController {
+    private func confiugreDataSource() {
+        self.postsDataSource = createDataSource()
+        self.configureSnapshot()
+    }
     
+    private func createDataSource() -> DataSource {
+        let cellRegistration = UICollectionView.CellRegistration<PostCell, PostDTO> { cell, indexPath, post in
+            cell.loadPost(post, indexPath)
+        }
+        
+        return DataSource(
+            collectionView: postsView.collectionView,
+            cellProvider: { collectionView, indexPath, id in
+                guard let post = self.posts?[indexPath.item] else {
+                    return UICollectionViewCell()
+                }
+                return collectionView.dequeueConfiguredReusableCell(
+                    using: cellRegistration,
+                    for: indexPath,
+                    item: post
+                )
+            }
+        )
+    }
 }
+
+// MARK: - Snapshot
+
+extension NewPostsViewController {
+    func configureSnapshot() {
+        self.postsSnapshot = Snapshot()
+        self.postsSnapshot.appendSections([.main])
+        postsDataSource.apply(self.postsSnapshot)
+    }
+    
+    func loadSnapshotData(
+        with incomingPosts: [PostDTO]
+    ) {
+        let postIDs = incomingPosts.map { $0.id }
+        self.postsSnapshot.appendItems(postIDs, toSection: .main)
+        postsDataSource.apply(self.postsSnapshot)
+    }
+}
+
+//struct PostModel: Identifiable {
+//    let id: UUID
+//    let post: PostDTO?
+//}
