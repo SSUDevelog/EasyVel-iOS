939aff34593273269dd62d5f20725b8b0d91c052
seongmin221 <seongmin221@naver.com>
2023-10-09 22:14:53 +0900

    [CHORE] #17 - added error color

--- EasyVel/Util/Extension/UIColor+Extension.swift
+++ EasyVel/Util/Extension/UIColor+Extension.swift
@@ -29,6 +29,9 @@ extension UIColor {
     static var gray700: UIColor {
         return UIColor(hex: "#212529")
     }
+    static var error: UIColor {
+        return UIColor(hex: "#FF4D4D")
+    }
 }
 
 extension UIColor {
